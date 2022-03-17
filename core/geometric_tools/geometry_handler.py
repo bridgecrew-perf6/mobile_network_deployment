ceb34eee1ff15e069a972f9367ab2c37d986d5a1
franciscocamellon <franciscocamellon@gmail.com>
2022-03-17 16:16:42 -0300

    end of the day commit

--- core/geometric_tools/geometry_handler.py
+++ core/geometric_tools/geometry_handler.py
@@ -149,3 +149,199 @@ class GeometryHandler(QObject):
             row += 1
 
         return polygons
+
+    def find_closest_site_areas(hexagons, geom_shape):
+    """
+
+    Get the transmitter and interfering site areas, by finding the closest
+    hex shapes. The first closest hex shape to the transmitter will be the
+    transmitter's site area. The next closest hex areas will be the
+    intefering site areas.
+
+    Parameters
+    ----------
+    hexagons : list of dicts
+        Each haxagon is a geojson dict.
+    geom_shape : Shapely geometry object
+        Geometry object for the transmitter.
+
+    Returns
+    -------
+    site_area : List of dicts
+        Contains the geojson site area for the transmitter.
+    interfering_site_areas : List of dicts
+        Contains the geojson interfering site areas.
+
+    """
+    idx = index.Index()
+
+    for site in hexagons:
+        coords = site['centroid']
+        idx.insert(0, coords.bounds, site)
+
+    transmitter = mapping(geom_shape.centroid)
+
+    site_area =  list(
+        idx.nearest(
+            (transmitter['coordinates'][0],
+            transmitter['coordinates'][1],
+            transmitter['coordinates'][0],
+            transmitter['coordinates'][1]),
+            1, objects='raw')
+            )[0]
+
+    closest_site_area_centroid = Polygon(
+        site_area['geometry']['coordinates'][0]
+        ).centroid
+
+    all_closest_sites =  list(
+        idx.nearest(
+            closest_site_area_centroid.bounds,
+            7, objects='raw')
+            )
+
+    interfering_site_areas = all_closest_sites[1:7]
+
+    site_area = []
+    site_area.append(all_closest_sites[0])
+
+    return site_area, interfering_site_areas
+
+
+    def find_site_locations(site_area, interfering_site_areas):
+        """
+
+        Get the centroid for each site area and intefering site areas.
+
+        Parameters
+        ----------
+        site_area : List of dicts
+            Contains the geojson site area for the transmitter.
+        interfering_site_areas : List of dicts
+            Contains the geojson interfering site areas.
+
+        Returns
+        -------
+        transmitter : List of dicts
+            Contains the geojson site location for the transmitter.
+        interfering_site_areas : List of dicts
+            Contains the geojson site locations for interfering sites.
+
+        """
+        site_area_site = Polygon(
+            site_area[0]['geometry']['coordinates'][0]
+            ).centroid
+
+        transmitter = []
+        transmitter.append({
+            'type': 'Feature',
+            'geometry': mapping(site_area_site),
+            'properties': {
+                'site_id': 'transmitter'
+            }
+        })
+
+        interfering_transmitters = []
+        for interfering_site in interfering_site_areas:
+            interfering_transmitters.append({
+                'type': 'Feature',
+                'geometry': mapping(interfering_site['centroid']),
+                'properties': {
+                    'site_id': interfering_site['properties']['site_id']
+                }
+            })
+
+        return transmitter, interfering_transmitters
+
+
+    def generate_site_areas(point, site_radius):
+        """
+
+        Generate a site area, as well as the interfering site areas, for
+        a specific site_radius.
+
+        Parameters
+        ----------
+        point : dict
+            Geojson point in desired Coordinate Reference System.
+        site_radius : int
+            Distance between transmitter and site edge in meters.
+
+        Returns
+        -------
+        site_area : List of dicts
+            Contains the geojson site area for the transmitter.
+        interfering_site_areas : List of dicts
+            Contains the geojson interfering site areas.
+
+        """
+        geom_shape = shape(point['geometry'])
+
+        buffered = Polygon(geom_shape.buffer(site_radius*2).exterior)
+
+        polygon = calculate_polygons(
+            buffered.bounds[0], buffered.bounds[1],
+            buffered.bounds[2], buffered.bounds[3],
+            site_radius)
+
+        hexagons = []
+        id_num = 0
+        for poly in polygon:
+            hexagons.append({
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Polygon',
+                    'coordinates': [poly],
+                },
+                'centroid': (Polygon(poly).centroid),
+                'properties': {
+                    'site_id': id_num
+                    }
+                })
+
+            id_num += 1
+
+        site_area, interfering_site_areas = find_closest_site_areas(
+            hexagons, geom_shape
+        )
+
+        return site_area, interfering_site_areas
+
+
+    def produce_sites_and_site_areas(unprojected_point, site_radius, unprojected_crs,
+        projected_crs):
+        """
+
+        Meta function to produce a set of hex shapes with a specific site_radius.
+
+        Parameters
+        ----------
+        unprojected_point : Tuple
+            x and y coordinates for an unprojected point.
+        site_radius : int
+            Distance between transmitter and site edge in meters.
+
+        Returns
+        -------
+        transmitter : List of dicts
+            Contains a geojson dict for the transmitter site.
+        interfering_transmitters : List of dicts
+            Contains multiple geojson dicts for the interfering transmitter sites.
+        site_area : List of dicts
+            Contains a geojson dict for the transmitter site area.
+        interfering_site_areas : List of dicts
+            Contains multiple geojson dicts for the interfering transmitter site
+            areas.
+
+        """
+        point = convert_point_to_projected_crs(unprojected_point, unprojected_crs,
+            projected_crs
+        )
+
+        site_area, interfering_site_areas = generate_site_areas(point, site_radius)
+
+        transmitter, interfering_transmitters = find_site_locations(site_area,
+            interfering_site_areas
+        )
+
+        return transmitter, interfering_transmitters, site_area, interfering_site_areas
